29a1213ef6ee5b6e679b88d9a7e2ba4f0ba5c608
salarissa321 <salarissa321@gmail.com>
2023-02-28 16:19:33 +0100

    add 2 python files

---
+++ python-exampels/python-Examples.py
@@ -0,0 +1,136 @@
+
+'''
+x= 5
+s = 'welcome'
+print(x,s , end='')
+print(' ')
+'''
+
+
+'''
+x,y= 5,3
+print(x,y)
+'''
+
+
+'''
+x= 5
+s = 'Salar Issa'
+print(type(x))
+print (type(s))
+'''
+
+
+'''
+x= 500
+if x > 600:
+    print(' i am the Winner')
+else:
+    print(' i am the Loser')
+'''
+
+
+'''
+x=100
+if x == 200:
+    print(' i am the King')
+elif x== 300:
+    print(' i am playing Football')
+elif x== 150:
+    print(' i am studying English')
+else:
+    print(' i am doing nothing')
+'''
+
+
+'''
+x=100
+if x < 200:
+    print(' x is less than 200')
+
+    if x== 150:
+        print('x=150')
+    elif x==50:
+        print('x=50')
+elif x < 50:
+    print('x is less than 50')
+'''
+
+
+'''
+name = 'salar'
+age = 27
+if name == 'salar' and age ==27:
+    print('salar issa')
+'''
+
+'''
+name = 'salar'
+age = 27
+if name == 'issa' or age== 27:
+    print(' Salar')
+'''
+
+
+'''
+x=100
+if x==100 : print(' i am hungrey')
+'''
+
+
+'''
+x= 5
+print(' i am playing playstation') if x==5 else print(' i am very happy')
+'''
+
+
+'''
+birds= {'parakeet' : 1, 'parrot' : 2}
+if 'parrot' in birds:
+    print('there is a parrot')
+'''
+
+'''
+birds= {' parakeet' : 1 , 'parrot' : 2}
+if 'salar' not in birds:
+    print('not found')
+'''
+
+
+'''
+x= 5
+y = 3
+z = 9
+if all([x==5 , y==3 ,z==9]):
+    print('all are true')
+'''
+
+'''
+x= 2
+y = 5
+r = 10
+if any ([x==2 , y==4 , r==7]):
+    print('not all are true')
+'''
+
+'''
+x= 1
+y = 5
+if x== 1 and y == 5:
+    print(True)
+if x==4 or y==5:
+    print(True)
+if not (x==1 and y==6):
+    print(True)
+if x != 0 and y !=4:
+    print(True)
+'''
+
+
+'''
+x=1
+if x in (2,0,4):
+    print('watch')
+else:
+    print('not found')
+'''
---
+++ python-exampels/python-tasks.py
@@ -0,0 +1,72 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+5 welcome
+
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+5 welcome
+
+
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+5 welcome
+ 
+
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+5 welcome 
+
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+5 3
+
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+<class 'int'>
+<class 'str'>
+
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+ i am the Loser
+
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+ i am doing nothing
+
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+ x is less than 200
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+salar isssa
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+salar issa
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+ Salar
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+ i am hungrey
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+ i am playing playstation
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+there is a parrot
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+not found
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+all are true
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+not all are true
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+True
+True
+True
+True
+>>> 
+========= RESTART: C:/Users/faten2/OneDrive/Desktop/python-Examples.py =========
+not found
